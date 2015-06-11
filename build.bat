b9277a67c29a1a6f28822919c0c6fd21e80f4208
fayedu39 <fayedu39@live.fr>
2015-06-11 18:06:57 +0200

    Changement du script de build

--- build.bat
+++ build.bat
@@ -1,2 +1,9 @@
-nasm src\boot\bootloader.asm -o bootloader.img
-nasm src\combo.asm -o KKrenel.img
+@echo off
+echo Entering source directory
+cd src
+echo Building bootloader
+nasm boot\bootloader.asm -o ..\bootloader.img
+echo Building whole kernel
+nasm combo.asm -o ..\KKrenel.img
+echo Going back in KK directory
+cd ..
